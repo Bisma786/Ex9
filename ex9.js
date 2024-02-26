3112804b95329119ffb8dad251a6506add7327c4
Bisma786 <159830145+Bisma786@users.noreply.github.com>
2024-02-26 21:21:06 +0500

    Initial commit

---
+++ ex9.js
@@ -0,0 +1,4 @@
+// Q:9 Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+var favnumber = 7;
+var message = "My favourite number is ".concat(7);
+console.log(message);
---
+++ ex9.ts
@@ -0,0 +1,6 @@
+// Q:9 Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favnumber:number=7
+let message:string=`My favourite number is ${7}`
+console.log(message)
+
